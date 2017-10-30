e45dc150a0686d6ee1ee886d4e0afa7725bd7aee
danielroberto8 <danieloberto81@gmail.com>
2017-10-30 16:25:02 +0700

    uts fail

---
+++ app/src/main/java/mobpro/inf/prak/danielcafe/second_main.java
@@ -0,0 +1,30 @@
+package mobpro.inf.prak.danielcafe;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by MU-05 on 30/10/2017.
+ */
+
+public class second_main {
+    Button logout = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_main);
+
+        logout = (Button)findViewById(R.id.btnLogout);
+    }
+
+    public  void logout(View view){
+        SharedPreferences sharedpreferences = getSharedPreferences(MainActivity.pref, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.clear();
+        editor.commit();
+    }
+}
